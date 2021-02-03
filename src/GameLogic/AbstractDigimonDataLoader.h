cdc17e041ce6d64659e6d2605a0990fe700549ef
Berat Özdemir <berational1991@gmail.com>
2021-02-03 04:29:55 +0100

    added comment

--- src/GameLogic/AbstractDigimonDataLoader.h
+++ src/GameLogic/AbstractDigimonDataLoader.h
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include "Digimon.h"
 //This Class provides an interface which reads the digimon Data from Progmem
-
 class AbstractDigimonDataLoader {
 public:
+    // reads the digimon Data from Progmem and returns a pointer to the data
     const DigimonProperties* getDigimonProperties(uint16_t digimonIndex);
 
 
